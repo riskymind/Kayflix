ceb136c36097cd23f5329769fcf2d1ee20efd6ff
kelechi <oparaiskelechi@gmail.com>
2025-03-10 23:22:51 +0100

    clean up navbar

--- frontend/src/pages/home/AuthPage.jsx
+++ frontend/src/pages/home/AuthPage.jsx
@@ -15,7 +15,7 @@ const AuthPage = () => {
 		<div className='hero-bg relative'>
 			{/* Navbar */}
 			<header className='max-w-6xl mx-auto flex items-center justify-between p-4 pb-10'>
-				<img src='/logo.png' alt='Logo' className='w-12 md:w-22' />
+				<img src='/logo.png' alt='Logo' className="h-8 dark:brightness-0 dark:invert rotate-0 hover:rotate-320 transition-all ease-in-out duration-700" />
 				<Link to={"/login"} className='text-white bg-red-600 py-1 px-2 rounded'>
 					Sign In
 				</Link>
