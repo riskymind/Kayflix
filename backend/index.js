93cbf1d9fe8fb4d1c9005786cce995846e8c0dbf
kelechi <oparaiskelechi@gmail.com>
2025-03-10 23:07:52 +0100

    good to go

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,12 @@ import movieRoute from "./routes/movie.route.js"
 import tvRoute from "./routes/tv.route.js"
 import serachRoute from './routes/search.route.js'
 import { protectRoute } from './middleware/protectRoute.js'
+import path from 'path'
 
 const app = express()
 
 const PORT = ENV_VARS.PORT
+const __dirname = path.resolve()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
@@ -21,7 +23,14 @@ app.use("/api/v1/movie", protectRoute, movieRoute)
 app.use("/api/v1/tv", protectRoute, tvRoute)
 app.use("/api/v1/search", protectRoute, serachRoute)
 
+if(ENV_VARS.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")))
+    app.get("*", (req, res)=> {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+    })
+}
+
 app.listen(PORT, ()=> {
-    console.log("Server is runing on port:" + PORT);
+    console.log("Server is runing at http://localhost:" + PORT);
     connectDB()
 })
